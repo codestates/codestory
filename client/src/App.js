c5f83bd6006bd7b5eb517b184ce0592d31de4740
Seoyong Lee <68191809+seoyong-lee@users.noreply.github.com>
2021-07-21 19:57:21 +0900

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from './components/Nav';
+import Ranking from './components/Ranking';
+import Profile from './components/Profile';
+import GameStart from './pages/GameStart';
+import Game from './components/Game';
+import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Switch>
+        <Route path="/gamestart">
+          <GameStart />
+        </Route>
+        <Route path="/ranking">
+          <Ranking />
+        </Route>
+        <Route path="/profile">
+          <Profile />
+        </Route>
+        <Route path="/game">
+          <Game />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
