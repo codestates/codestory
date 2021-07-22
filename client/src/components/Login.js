61a61e2f07881ec4e70fc9030720a0745e90e12c
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-07-22 13:14:17 +0900

    Fix indentation Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,14 +11,14 @@ function Login() {
           <input id="login-input-password" placeholder="비밀번호"></input>
           <p id="login-valid">비밀번호를 입력해 주세요</p>
           <button id="login-btn">로그인</button>
-            <div id="login-social">
-              <a className="login-social-btn" href="">
-                <img className="login-social-image" src="login-google.png" alt="google"/>
-              </a>
-              <a className="login-social-btn" href="">
-                <img className="login-social-image" src="login-kakao.png" alt="kakao"/>
-              </a>
-            </div>
+          <div id="login-social">
+            <a className="login-social-btn" href="">
+              <img className="login-social-image" src="login-google.png" alt="google"/>
+            </a>
+            <a className="login-social-btn" href="">
+              <img className="login-social-image" src="login-kakao.png" alt="kakao"/>
+            </a>
+          </div>
           <a id="login-signin" href="">아직 아이디가 없으신가요?</a>
         </div>  
       </div>
