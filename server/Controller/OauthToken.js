b9cac57d56fb6c7e134e77492809863673c14089
Candyroom136 <candyroom136@naver.com>
2021-07-22 11:28:03 +0900

    Configure database setting
    1. database연결 코드 작성
    2. users, follower_followeds,scripts 테이블 create

--- server/Controller/OauthToken.js
+++ server/Controller/OauthToken.js
@@ -1,6 +1,7 @@
 module.exports = {
     OauthFunctionOne: async (req, res) => {
       //OauthFunctionOne함수를 채워주세요. 함수명 바꿀수 있습니다.
+      //push를 위한 의도적 변경입니다.
       return res.status(200).send('i"m oauth');
     }
   };
