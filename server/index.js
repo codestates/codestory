f420ab726e639f24a4846fe30b6fbf0e42ec8d28
ruleBased848 <rkdfowns@naver.com>
2021-07-22 21:52:22 +0900

    Change port number

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
-require("dotenv").config();
-const fs = require("fs");
-const https = require("https");
-const cors = require("cors");
-const express = require("express");
+require('dotenv').config();
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+const express = require('express');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: true,
     credentials: true,
-    methods: ["GET", "POST", "PATCH","DELETE","PUT"],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'PUT'],
   })
 );
 
@@ -20,9 +20,9 @@ const gameRouter = require('./Router/GameRouter');
 const rankingRouter = require('./Router/RankingRouter');
 const signInRouter = require('./Router/SignInRouter');
 const signOutRouter = require('./Router/SignOutRouter');
-const signUpRouter= require('./Router/SignUpRouter')
+const signUpRouter= require('./Router/SignUpRouter');
 const userInfoRouter=require('./Router/UserInfoRouter');
-const HTTPS_PORT = process.env.HTTPS_PORT || 4000;
+const HTTPS_PORT = process.env.HTTPS_PORT || 80;
 
 app.use('/follow', followRouter);
 app.use('/game', gameRouter);
@@ -31,9 +31,9 @@ app.use('/signin', signInRouter);
 app.use('/signout', signOutRouter);
 app.use('/signup', signUpRouter);
 app.use('/userinfo',  userInfoRouter);
-app.get("/",(req,res)=>{
-  res.json("Hello World!");
-})
+app.get('/',(req,res)=>{
+  res.json('Hello World!');
+});
 
 
 
@@ -42,14 +42,14 @@ app.get("/",(req,res)=>{
 // 만약 인증서 파일이 존재하지 않는경우, http 프로토콜을 사용하는 서버를 실행합니다.
 // 파일 존재여부를 확인하는 폴더는 서버 폴더의 package.json이 위치한 곳입니다.
 let server;
-if(fs.existsSync("./key.pem") && fs.existsSync("./cert.pem")){
+if(fs.existsSync('./key.pem') && fs.existsSync('./cert.pem')){
 
-  const privateKey = fs.readFileSync(__dirname + "/key.pem", "utf8");
-  const certificate = fs.readFileSync(__dirname + "/cert.pem", "utf8");
+  const privateKey = fs.readFileSync(__dirname + '/key.pem', 'utf8');
+  const certificate = fs.readFileSync(__dirname + '/cert.pem', 'utf8');
   const credentials = { key: privateKey, cert: certificate };
 
   server = https.createServer(credentials, app);
-  server.listen(HTTPS_PORT, () => console.log("server runnning"));
+  server.listen(HTTPS_PORT, () => console.log('server runnning'));
 
 } else {
   server = app.listen(HTTPS_PORT, () => {
