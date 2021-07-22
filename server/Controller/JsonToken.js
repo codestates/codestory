9cbd407fb517958ea37df09f79a8343654195688
Candyroom136 <candyroom136@naver.com>
2021-07-23 08:55:58 +0900

    Fix jsonwebtoken function
    jsonwebtoken 함수에서 필요없는 주석을 제거했습니다.

--- server/Controller/JsonToken.js
+++ server/Controller/JsonToken.js
@@ -6,12 +6,9 @@ module.exports = {
     return sign(data, process.env.ACCESS_SECRET, { expiresIn: "3600s" });
   },
   sendAccessToken: (res, accessToken) => {
-    // TODO: JWT 토큰을 쿠키로 전달합니다.
     res.cookie('jwtAccessToken',accessToken).status(200).json({message: "ok" });
   },
   isAuthorizedJwt: (req) => {
-    // TODO: JWT 토큰 정보를 받아서 검증합니다.
-    // HINT: jsonwebtoken 라이브러리의 verify 함수를 사용하여 decode된 payload를 리턴하세요. (공식 문서의 Synchronous한 방법을 사용합니다)
     if(req.cookies){
       const jwt = req.cookies.jwtAccessToken;
       if (!jwt) {
