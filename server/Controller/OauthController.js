16929a5cf0f6ee452e6b8254ffa9cdee3037794e
Candyroom136 <candyroom136@naver.com>
2021-07-24 14:50:06 +0900

    merge upstream dev to origin feature_serveroauth

--- server/Controller/OauthController.js
+++ server/Controller/OauthController.js
@@ -3,21 +3,9 @@ const {getKaKaoToken,getGoogleToken} = require('./OauthToken');
 
 
 module.exports = {
-  googleLogin: async (req, res) => {
-    try {
-        console.log(req.body);   
-        const authorizationCode=req.body.authorizationCode;
-        res.status(200).send("google login 라우팅")
-    }
-    catch (error) {
-      res.status(500).send({ message: 'Sorry Can\'t process your request' });
-      throw error;
-    }
-  },
   oauthLogin: async(req,res) => {
     try {
-        console.log(req.body);   
-        // getKaKatoToken(req)
+        
         res.status(200).json({"oauthAccessToken":"hi new token"})
     }
     catch (error) {
