d7a5632d52acc757f28e5ef3a96cc2bd4755472b
Kyung Il Jo <81741466+satoshi25@users.noreply.github.com>
2021-07-25 13:17:22 +0900

    Add API connection to SignUp component
    회원가입 API 연결
    두 비밀번호가 같지 않을시에 경고 메세지
    로그인 경고메세지 줄 맞춤

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,23 +1,32 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import '../css/signup.css';
+import axios from 'axios';
 
-function SignUp( onLoginClick ) {
+function SignUp( { signupHandler } ) {
 
   const [currentId, setCurrentId] = useState({ value: '', valid: false });
   const [currentPassword, setCurrentPassword] = useState({ value: '', valid: false });
   const [currentPassword2, setCurrentPassword2] = useState({ value: '', valid: false });
-  const [validUser, setvalidUser] = useState({ id: '', password: '' });
+  const [validUser, setvalidUser] = useState({ username: '', password: '' });
+  const [errorMessage, setErrorMessage] = useState('');
+  const history = useHistory();
+
+  useEffect(() => {
+    if (validUser.username !== '' && validUser.password !== '') {
+      return createUser();
+    }
+  },[validUser]);
 
   const inputId = (e) => {
     const input = e.target.value;
     if (input.length > 2) {
-      setCurrentId({...currentId, value: input, valid: true});
+      setCurrentId({ ...currentId, value: input, valid: true });
     } else {
-      setCurrentId({...currentId, value: input, valid: false});
+      setCurrentId({ ...currentId, value: input, valid: false });
     }
   };
-
+  
   const inputPassword = (e) => {
     const input = e.target.value;
     if (input.length > 7) {
@@ -35,13 +44,29 @@ function SignUp( onLoginClick ) {
       setCurrentPassword2({...currentPassword2, value: input, valid: false});
     }
   };
- 
+  
   const userValidator = () => {
-    if (currentId && currentPassword) {
-      setvalidUser({...validUser, id: currentId.value, password: currentPassword.value });
-      // axios.post()
-      console.log(validUser);
+    if (currentId && currentPassword2.valid) {
+      return setvalidUser({ ...validUser, username: currentId.value, password: currentPassword2.value });
+    } else {
+      setErrorMessage('비밀번호가 일치하지 않습니다');
     }
+  };
+  
+  const createUser = async () => {
+    const { username, password } = validUser;
+    await axios.post('https://api.codestory.academy/user', {
+      username: username,
+      password: password
+    }, {
+      'content-type': 'application/json',
+      withCredentials: true
+    }).then((result) => {
+      console.log(result.data);
+      history.push('/gamestart');
+    }).catch((err) => {
+      console.log(err);
+    });
   };
 
   return (
@@ -49,18 +74,18 @@ function SignUp( onLoginClick ) {
       <object id="signup-logo" type="image/svg+xml" data="logo.svg" aria-label="logo"></object>
       <div id="signup-container">
         <div id="signup-wrapper">
-          <input id="signup-input-id" value={currentId.value} placeholder="아이디 (3자리 이상)" onChange={(e)=>inputId(e)}></input>
+          <input id="signup-input-id" value={currentId.value} placeholder="아이디 (3자리 이상)" onChange={(e) => inputId(e)}></input>
           { currentId.valid ? <div id="signup-valid">사용할 수 있는 아이디입니다</div> : <div id="signup-invalid">아이디를 확인해 주세요</div>}
-          <input id="signup-input-password" type='password' value={currentPassword.value} placeholder="비밀번호 (8자리 이상)" onChange={(e)=>inputPassword(e)}></input>
+          <input className="signup-input-password" type='password' value={currentPassword.value} placeholder="비밀번호 (8자리 이상)" onChange={(e)=>inputPassword(e)}></input>
           { currentPassword.valid ? <div id="signup-valid">비밀번호가 유효합니다</div> : <div id="signup-invalid">비밀번호를 확인해 주세요</div>}
-          <input id="signup-input-password" type='password' value={currentPassword2.value} placeholder="비밀번호 확인" onChange={(e)=>inputPassword2(e)}></input>
+          <input className="signup-input-password" type='password' value={currentPassword2.value} placeholder="비밀번호 확인" onChange={(e)=>inputPassword2(e)}></input>
           { currentPassword2.valid ? <div id="signup-valid">비밀번호가 유효합니다</div> : <div id="signup-invalid">비밀번호를 확인해 주세요</div>}
-          <button id="signup-btn" onClick={()=>userValidator()}>
-            <Link to="/gamestart">
-              회원가입
-            </Link>
+          <button id="signup-btn" onClick={() => userValidator()}>
+            회원가입
           </button>
-          <a id="signup-signin" style={{ cursor: 'pointer' }} onClick={()=>onLoginClick.props()}>이미 아이디가 있으신가요?</a>
+          {errorMessage === '' ? null :
+            <div className="warn-box">{errorMessage}</div>}
+          <a id="signup-signin" style={{ cursor: 'pointer' }} onClick={() => signupHandler()}>이미 아이디가 있으신가요?</a>
         </div>  
       </div>
     </div>
