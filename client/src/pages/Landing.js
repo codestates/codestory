b0ebf2d507e6117c5a4045cc6bbdc8a844dd4702
seoyong-lee <sy3783@gmail.com>
2021-07-27 00:14:13 +0900

    Modify landing page scroll movement
    로고와 글자 클릭을 통해 다음으로 넘어가도록 스크롤 이동 구현

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -47,29 +47,46 @@ function Landing({loginClick, setIsLogin}) {
     transition: '.5s'
   };
 
-  const move = () => {
-    document.getElementById('root').scrollTo(0, 800);
+  const move1 = () => {
+    window.scrollTo({
+      top: 670,
+      behavior: 'smooth'
+    });
+  };
+
+  const move2 = () => {
+    window.scrollTo({
+      top: 1350,
+      behavior: 'smooth'
+    });
+  };
+
+  const move3 = () => {
+    window.scrollTo({
+      top: 2030,
+      behavior: 'smooth'
+    });
   };
 
   return (
     <div id="landing-container">
       <div id="landing-page-1">
-        <div id="landing-logo-box" onClick={move}>
+        <div id="landing-logo-box" onClick={move1}>
           <object id="landing-logo" className="wow pulse animate__slower" type="image/svg+xml" data="logo.svg" aria-label="logo"></object>
         </div>
       </div>
       <div id="landing-page-2">
-        <div id="landing-word" className="wow pulse">아직도 코딩을 공부하고 계신가요?</div>
+        <div id="landing-word" className="wow pulse" onClick={move2}>아직도 코딩을 공부하고 계신가요?</div>
         <button className='landing-btn' style={isVisible ? fadeIn : fadeOut} onClick={()=>scrollToBottom()}>
           로그인 하러가기
           <span className="material-icons" id="landing-btn-arrow">expand_more</span>
         </button>
       </div>
       <div id="landing-page-3">
-        <div id="landing-word" className="wow pulse">혹시 코딩 공부를 더 재미있게 하고 싶진 않으신가요?</div>
+        <div id="landing-word" className="wow pulse" onClick={move3}>혹시 코딩 공부를 더 재미있게 하고 싶진 않으신가요?</div>
       </div>
       <div id="landing-page-4">
-        <div id="landing-word" className="wow pulse">스토리로 배우는 코딩! CodeStory와 함께하세요</div>
+        <div id="landing-word" className="wow pulse" onClick={scrollToBottom}>스토리로 배우는 코딩! CodeStory와 함께하세요</div>
       </div>
       <div id="landing-page-5">
         <Login loginClick={loginClick}/>
