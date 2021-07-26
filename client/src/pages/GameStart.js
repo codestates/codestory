681bfa43d1373452df73affa3c417358af358230
seoyong-lee <sy3783@gmail.com>
2021-07-27 00:00:11 +0900

    Modify login checker

--- client/src/pages/GameStart.js
+++ client/src/pages/GameStart.js
@@ -5,8 +5,8 @@ import Footer from '../components/Footer';
 import '../css/gamestart.css';
 
 
-function GameStart({loginClick, setIsLogin}) {
-
+function GameStart({loginClick}) {
+  
   const url = new URL(window.location.href);
   const authorizationCode = url.searchParams.get('code'); 
   const [isHover, setIsHover] = useState(false);
@@ -24,15 +24,9 @@ function GameStart({loginClick, setIsLogin}) {
     }
   },[]);
   
-  useEffect(()=> {
-    axios.get('https://api.codestory.academy/user', {
-      'content-type': 'application/json',
-      withCredentials: true
-    }).then(()=>{
-      setIsLogin(true);
-    });
-  });
+
   
+
   const onMouseOver = () => {
     setIsHover(true);
   };
